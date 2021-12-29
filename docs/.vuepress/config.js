d5b9d292ec61e24c5290beeee42c7e574b23aa97
xugaoyi <894072666@qq.com>
2021-12-29 18:53:07 +0800

    feat: 添加常用轮子等内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,7 +28,14 @@ module.exports = {
         ],
       },
       // { text: '免费视频', link: '/pages/859f95/' },
-      { text: '资源分享', link: '/resource/' },
+      {
+        text: '前端资源',
+        link: '/resource/',
+        items: [
+          { text: '前端资源', link: '/resource/' },
+          { text: '常用轮子', link: '/wheel/' },
+        ],
+      },
       { text: '学习技巧', link: '/skill/' },
       { text: '常用工具', link: '/tools/' },
       { text: '摸鱼时间', link: '/fish/' },
@@ -135,7 +142,9 @@ module.exports = {
         <div class="line-between"></div>
         <div class="article-footer">
           <div class="block-title">邀您共建知识库</div>
-          <div class="block-content">如果您有不错的资源，欢迎在留言区留言。</div>
+          <div class="block-content">如果您有不错的资源，欢迎在 <b>留言区</b> 或
+          <a href="https://github.com/xugaoyi/web-learn/discussions/categories/%E5%88%86%E4%BA%AB%E8%B5%84%E6%BA%90" target="_blank" rel="noopener noreferrer">分享资源区<span><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg> <span class="sr-only">(opens new window)</span></span></a>
+          留言。</div>
         </div>
       `
     } // 插入hmtl(广告)模块
